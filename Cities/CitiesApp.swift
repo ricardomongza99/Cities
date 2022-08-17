ca4b56ea1052a159041271784d0f80d131f4ded3
Ricardo Montemayor <ricardomongza@gmail.com>
2022-08-17 11:33:02 -0500

    Initial Commit

---
+++ Cities/CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesApp.swift
+//  Cities
+//
+//  Created by Ricardo Montemayor on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
